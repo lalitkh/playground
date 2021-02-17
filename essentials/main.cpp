92f3c29f6fff703b435123bd433c20d1c8f6e4ab
Lalit Khandelwal <lalit871416@gmail.com>
2021-02-16 23:48:35 -0600

    Initial Commit

---
+++ essentials/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  essentials
+//
+//  Created by Lalit Khandelwal on 2/16/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
