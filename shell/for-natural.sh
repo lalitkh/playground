40e1cef21d756565f0b91ecdddd0e99630e72557
Lalit Khandelwal <lalit871416@gmail.com>
2021-06-11 21:35:09 +0530

    refactored

--- shell/for-natural.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for n in {1..50}
-do  
-    echo $n
-done
--- shell/world_of_numbers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter first number : " x
-read -p "Enter second number : " y
-
-echo "Addition: $((x+y))"
-echo "Substraction: $((x-y))"
-echo "Multiplication: $((x*y))"
-echo "Division: $((x/y))"
